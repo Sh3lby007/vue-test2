2319478ce3295268f70c152319a679572aaeed99
Sh3lby007 <zhengshaobin@ymail.com>
2022-11-05 18:17:31 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script>
+import Block from './components/Block.vue'
+
+export default {
+  name: 'App',
+  components: { Block },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random * 5000
+      this.isPlaying = true
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>Ninja Reaction timer</h1>
+  <button @click="start" :disabled="isPlaying">play</button>
+  <Block v-if="isPlaying" :delay="delay" />
+
+</template>
+
+<style scoped>
+#app {
+  font-family: Arial, Helvetica, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Block.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="block" v-if="showBlock" @click="stopTimer">
+        Click me
+    </div>
+
+</template>
+
+<script>
+export default {
+    props: ['delay'],
+    data() {
+        return {
+            showblock: false,
+            timer: null,
+            reactionTime: 0
+
+        }
+    },
+    mounted() {
+        setTimeout(() => {
+            this.showblock = true
+            this.startTimer()
+        }, this.delay)
+    },
+    methods: {
+        startTimer() {
+            this.timer = setInterval(() => {
+                this.reactionTime += 10
+            }, 10)
+        },
+        stopTimer() {
+            clearInterval(this.timer)
+
+        }
+    }
+}
+</script>
